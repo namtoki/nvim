3ff67666b26c7d72ac34bf8503e8a24ae28dc291
Toshiyuki Tanamura <toshi.turner@gmail.com>
2025-06-13 13:45:33 +0900

    up

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,21 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       autoformat = false,
+      servers = {
+        sourcekit = {
+          cmd = { "xcrun", "sourcekit-lsp" },
+          filetypes = { "swift", "objective-c", "objective-cpp" },
+          root_dir = require("lspconfig.util").root_pattern("Package.swift", ".git", "*.xcodeproj", "*.xcworkspace"),
+          autoformat = false,
+        },
+      },
+      setup = {
+        sourcekit = function(_, opts)
+          opts.on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end,
+      },
     },
   },
   { "headlines.nvim", enabled = false },
